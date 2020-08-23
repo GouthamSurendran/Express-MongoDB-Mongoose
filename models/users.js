9a3ffa11080eeb0a48927f3cfc330a82cfade274
GouthamSurendran <gouthu.g3@gmail.com>
2020-08-23 13:17:20 +0530

    Mongoose Population

--- models/users.js
+++ models/users.js
@@ -3,12 +3,20 @@ const Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    admin:{
-        type:Boolean,
-        default:false
+    firstname: {
+        type: String,
+        default: ''
+    },
+    lastname: {
+        type: String,
+        default: ''
+    },
+    admin: {
+        type: Boolean,
+        default: false
     }
 });
 
 User.plugin(passportLocalMongoose); //sets up passport authentication schema on User schema
 
-module.exports = mongoose.model('User',User);
+module.exports = mongoose.model('User', User);

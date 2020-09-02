c27aab9f9361ccf3b06451f64ca86e945015b893
GouthamSurendran <gouthu.g3@gmail.com>
2020-09-02 20:40:54 +0530

    Assignment 4

--- routes/favouriteRouter.js
+++ routes/favouriteRouter.js
@@ -64,6 +64,74 @@ favouriteRouter.route('/')
                 res.json(favourite);
             }, (err) => next(err))
             .catch((err) => next(err));
+    });
+
+favouriteRouter.route('/:dishId')
+    .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
+    .get(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('GET operation not supported on /favourites/' + req.params.dishId);
     })
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favourites.findOne({ user: req.user._id })
+            .then((favourite) => {
+                if (favourite) {
+                    if (favourite.dishes.indexOf(req.params.dishId) === -1) {
+                        favourite.dishes.push(req.params.dishId);
+                    }
+                    favourite.save()
+                        .then((favourite) => {
+                            console.log('Favourite Added ', favourite);
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favourite);
+                        }, err => next(err));
+                }
+                else {
+                    Favourites.create({ 'user': req.user._id, 'dishes': [req.params.dishId] })
+                        .then((favourite) => {
+                            console.log('Favourite Added ', favourite);
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'application/json');
+                            res.json(favourite);
+                        }, err => next(err));
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err))
+    })
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        res.statusCode = 403;
+        res.end('PUT operation not supported on /favorites/' + req.params.dishId);
+    })
+    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+        Favourites.findOne({ user: req.user._id })
+            .then((favourite) => {
+                if (favourite) {
+                    index = favourite.dishes.indexOf(req.params.dishId);
+                    if (index >= 0) {
+                        favourite.dishes.splice(index, 1); // Delete 1 element from position index
+                        favourite.save()
+                            .then((favourite) => {
+                                console.log('Favourite Deleted ', favourite);
+                                res.statusCode = 200;
+                                res.setHeader('Content-Type', 'application/json');
+                                res.json(favourite);
+                            }, err => next(err));
+                    }
+                    else {
+                        err = new Error('Dish ' + req.params.dishId + ' not found!');
+                        err.status = 404;
+                        return next(err);
+                    }
+                }
+                else {
+                    err = new Error('No Favourites Found!');
+                    err.status = 404;
+                    return next(err);
+                }
+            }, (err) => next(err))
+            .catch((err) => next(err));
+    })
+
 
 module.exports = favouriteRouter;   
